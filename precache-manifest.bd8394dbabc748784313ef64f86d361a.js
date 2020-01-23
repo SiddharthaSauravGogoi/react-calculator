c28e5457851d1537692bd6f9713393f624cea79e
SiddharthaSauravGogoi <lezaroth3@gmail.com>
2020-01-23 21:00:41 +0530

    Updates

---
+++ precache-manifest.bd8394dbabc748784313ef64f86d361a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d7eef82f2ce53628ad0afee43b8f45",
+    "url": "/SiddharthaSauravGogoi/react-calculator/index.html"
+  },
+  {
+    "revision": "98e2c966f676494657ad",
+    "url": "/SiddharthaSauravGogoi/react-calculator/static/css/main.f98c2f3e.chunk.css"
+  },
+  {
+    "revision": "d2955fc2fb7ce5111cb0",
+    "url": "/SiddharthaSauravGogoi/react-calculator/static/js/2.61040d5a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SiddharthaSauravGogoi/react-calculator/static/js/2.61040d5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98e2c966f676494657ad",
+    "url": "/SiddharthaSauravGogoi/react-calculator/static/js/main.1a7ce953.chunk.js"
+  },
+  {
+    "revision": "e0f57b11039539eaa02f",
+    "url": "/SiddharthaSauravGogoi/react-calculator/static/js/runtime-main.f29e9478.js"
+  }
+]);
